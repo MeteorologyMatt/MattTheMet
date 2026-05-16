0e1452206ecc8341f5ddb7ddf8d19ce7870c16ef
MeteorologyMatt <mwl140@msstate.edu>
2026-05-16 16:38:59 -0500

    Refactor routes in App component for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PersonalWebsite from './PersonalWebsite.jsx';
-import Sandy from './pages/sandy.jsx'; 
+
+// Import all of your sub-pages
+import Home from './pages/Home.jsx';
+import Experience from './pages/Experience.jsx';
+import Research from './pages/Research.jsx';
+import Contact from './pages/Contact.jsx';
+import Survey from './pages/Survey.jsx';
+import Sandy from './pages/sandy.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 export default function App() {
   return (
     <Routes>
-      {/* Your main portfolio loads at the default root path */}
+      {/* If PersonalWebsite acts as your full single-page landing layout */}
       <Route path="/" element={<PersonalWebsite />} />
       
-      {/* Your new Sandy analysis loads at /#/sandy */}
+      {/* Explicit routes for individual sub-pages */}
+      <Route path="/home" element={<Home />} />
+      <Route path="/experience" element={<Experience />} />
+      <Route path="/research" element={<Research />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/survey" element={<Survey />} />
       <Route path="/sandy" element={<Sandy />} />
+      
+      {/* Catch-all route for any typos or broken links */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
