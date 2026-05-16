ce7c7c6d8104c6bd555a0daf38e1a937dc1b9af9
MeteorologyMatt <mwl140@msstate.edu>
2026-05-16 16:29:06 -0500

    Fix import path for Sandy component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import PersonalWebsite from './PersonalWebsite.jsx';
-import Sandy from 'MattTheMet-backup/src/pages/sandy.jsx'; // Ensure this matches the exact folder and file name you created
-
+import Sandy from './pages/sandy.jsx';
 export default function App() {
   return (
     <Router>
