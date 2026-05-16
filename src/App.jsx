8abae4ff70a7669a8526fc3008a4c15557854fdb
MeteorologyMatt <mwl140@msstate.edu>
2026-05-16 15:02:12 -0500

    Refactor App component to use PersonalWebsite

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,4 @@
-// Inside src/PersonalWebsite.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HurricaneSandyPage from './pages/HurricaneSandyPage'; // Add this import
-import Research from './pages/Research';
-// ... your other imports
-
-export default function PersonalWebsite() {
-  return (
-    <Router>
-      <Routes>
-        {/* Your other routes */}
-        <Route path="/research" element={<Research />} />
-        
-        {/* Add the new route for your Sandy project */}
-        <Route path="/research/hurricane-sandy" element={<HurricaneSandyPage />} />
-      </Routes>
-    </Router>
-  );
+import PersonalWebsite from "./PersonalWebsite.jsx";
+export default function App() {
+  return <PersonalWebsite />;
 }
