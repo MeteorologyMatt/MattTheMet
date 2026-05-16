0839ff7a1e46cb7ae10b339aac4752eeb82800fa
MeteorologyMatt <mwl140@msstate.edu>
2026-05-16 16:36:12 -0500

    Remove Router component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import PersonalWebsite from './PersonalWebsite.jsx';
-import Sandy from './pages/sandy.jsx';
+import Sandy from './pages/sandy.jsx'; 
+
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Your main portfolio loads at the default root path */}
-        <Route path="/" element={<PersonalWebsite />} />
-        
-        {/* Your new Sandy analysis loads at /#/sandy */}
-        <Route path="/sandy" element={<Sandy />} />
-      </Routes>
-    </Router>
+    <Routes>
+      {/* Your main portfolio loads at the default root path */}
+      <Route path="/" element={<PersonalWebsite />} />
+      
+      {/* Your new Sandy analysis loads at /#/sandy */}
+      <Route path="/sandy" element={<Sandy />} />
+    </Routes>
   );
 }
