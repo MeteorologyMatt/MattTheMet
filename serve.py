0d7a95410b041144433a836f35f134ab5e9e337d
MeteorologyMatt <mwl140@msstate.edu>
2026-05-16 15:58:47 -0500

    Delete serve.py

--- serve.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import http.server, socketserver, os
-PORT = 8000
-os.chdir(os.path.join(os.path.dirname(__file__), 'live-sat'))
-Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving live-sat/ at http://localhost:{PORT}/")
-    httpd.serve_forever()
