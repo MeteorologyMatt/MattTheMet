41e68ae679672115a1a43e69f873f641b71aea89
MeteorologyMatt <mwl140@msstate.edu>
2026-05-17 12:47:21 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PersonalWebsite from './PersonalWebsite.jsx';
-import Sandy from './pages/sandy.jsx'; 
-import SandyCode from './pages/SandyCode.jsx'; 
 
 export default function App() {
   return (
     <Routes>
-      {/* Standalone route for your Hurricane Sandy analysis page */}
-      <Route path="/sandy" element={<Sandy />} />
-      <Route path="/sandy-code" element={<SandyCode />} /> {/* 2. Add this line */}
-      {/* The wildcard "/*" passes all other portfolio paths (like /research, /experience, etc.)
-          back to your main layout so they render with the navbar and theme styling intact! */}
+      {/* Send literally every link directly to your main layout component */}
       <Route path="/*" element={<PersonalWebsite />} />
     </Routes>
   );
